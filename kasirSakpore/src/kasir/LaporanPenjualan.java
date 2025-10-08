4badcedd3c23b2c769f9fdbc7a2b2316ce7147a4
gon <ramdhanivirgan@gmail.com>
2025-10-08 08:37:25 +0700

    aaaa

--- kasirSakpore/src/kasir/LaporanPenjualan.java
+++ kasirSakpore/src/kasir/LaporanPenjualan.java
@@ -312,8 +312,8 @@ public class CustomFocusTraversalPolicy extends FocusTraversalPolicy {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 954, Short.MAX_VALUE)
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
